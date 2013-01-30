7843bcf0a0e1c54d319db25878efa0a9c848ad6d
Taylor Eisman <tayloreisman@fullsail.edu>
2013-01-29 20:43:54 -0600

    Pasting old js file to make changes to make it work
    please commit!

--- js/main.js
+++ js/main.js
@@ -107,3 +107,141 @@ $( '#remoteData' ).on('pageinit', function(){
     });
 
 });
+
+
+$('#addBill').on('pageinit', function(){
+
+		var myForm = $('#addBill');
+		    myForm.validate({
+			invalidHandler: function(form, validator) {
+			},
+			submitHandler: function() {
+		var data = myForm.serializeArray();
+			storeData(data);
+		}
+	});
+	
+	//any other code needed for addItem page goes here
+	
+});
+
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
+
+var autofillData = function (){
+	 .each(var n in json){
+			var id = Math.floor(Math.random()*100000001);
+			localStorage.setItem(id, JSON.stringify(json[n]));
+		}
+};
+
+var getData = function(){
+	$("#addBill").empty();
+		toggleControls("on");
+		if(localStorage.length === 0){
+			alert("There is no Local Storage so default data was added.");
+			autoFillData();
+		}
+		var makeDiv = $('<div>');
+		makeDiv.attr("id", "items");
+		var makeList = $('<ul>');
+		makeDiv.append(makeList);
+		document.body.appendChild(makeDiv);
+		$('#bills').append(makeDiv);
+		$('#items').show();
+		for(var i=0, len=localStorage.length; i<len;i++){
+			var makeli = $('<li>');
+			var linksLi = $('<li>');
+			makeList.append(makeli);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			var obj = JSON.parse(value);
+			var makeSubList = $('<ul>');
+			makeli.append(makeSubList);
+			getImage(obj.category[1], makeSubList);
+			for(var n in obj){
+				var makeSubli = $('<li>');
+				makeSubList.append(makeSubli);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubli.text(optSubText);
+				makeSubList.append(linksLi);
+			}
+			makeItemLinks(localStorage.key(i), linksLi);
+		}
+
+};
+
+
+
+
+
+var storeData = function(data){
+	if(!key){
+			var id			= Math.floor(Math.random()*100000001);
+		}else{
+			id = key;
+		}
+		getSelectedRadio();
+		getCheckboxValue();
+		
+		var item					= {};
+			item.compName			= ["Company Name: ", $('#compName').val()];
+			item.category			= ["Category: ", $('#category').val()];
+			item.compEmail		= ["Company Email: ", $('#compEmail').val()];
+			item.compWeb		= ["Company Website: ", $('#compWeb').val()];
+			item.payBy			= ["PayBy: ", payByValue];
+			item.pastDue			= ["Past Due: ", pastDue];
+			item.budgetPercent		=["Budget Percent: " , $('#budgetPercent').value];
+			item.date			= ["Date Added: ", $('#date').value];
+			item.notes		= ["Notes: ", $('#notes').val()];
+		
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Bill Saved!");
+		save.off("click");
+		save.on("click", storeData);
+		window.location.reload();
+	
+	};
+	
+	var getSelectedRadio = function(){
+	var radios = function (){
+		$('input:radio[name="addToFav"]:checked').val();
+		return($('input:radio[name="addToFav"]:checked').val());
+
+};
+
+}; 
+
+var	deleteItem = function (){
+	var ask = confirm("Are you sure you want to delete this bill?");
+		if(ask){
+			localStorage.removeItem(this.key);
+			alert("Bill was deleted.");
+			window.location.reload();
+		}else{
+			alert("Bill was not deleted.");
+		}		
+};
+					
+var clearLocal = function(){
+	if(localStorage.length === 0){
+			alert("No data to clear.");
+		}else{
+			localStorage.clear();
+			alert("All Bills are deleted");
+			window.location.reload();
+			return false;
+		}
+	}
+	
+	var pastDueValue,
+		pastDueValue = "No",
+		errMsg = ge('errors');
+	
+	makeCats();
+
+	var clearLink = ge('clear');
+	clearLink.addEventListener("click", clearLocal);
+	var save = ge('submit');
+	save.addEventListener("click", validate);
+
+};
