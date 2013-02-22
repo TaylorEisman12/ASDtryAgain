5eb2f25298ee50440a16df947f94cba362d2289c
Taylor Eisman <TaylorEisman@fullsail.edu>
2013-02-21 21:30:07 -0600

    Working on couch data to load into my app.

---
+++ asdcouch/app/_attachments/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+	$.ajax({
+		"url": "_view/bills",
+		"dataType": "json",
+		"success": function(data) {
+			$.each(data.rows, function(index, bill){
+				var compName = bill.value.compName;
+				var compWeb = bill.value.compWeb;
+				var category = bill.value.category;
+				var compEmail = bill.value.compEmail;
+				var pastDue = bill.value.pastDue;
+				var payBy = bill.value.payBy;
+				var budgetPercent = bill.value.budgetPercent;
+				var notes = bill.value.notes;
+				var date = bill.value.date;
+				$('#couchList').append(
+					$('<li>').append(
+						$('<a>').attr("href", "#")
+							.text(compName)
+					)
+				);
+			});
+			$('#couchList').listview('refresh');
+		}
+	});
+});
+
+
+CHANGE TO DISPLAY CATEGORIES AND THEN SHOW BILLS WITHIN EACH CATEGORY
